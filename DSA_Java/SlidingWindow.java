7ecb03fb75862bb610ff18af63b0f136d2d87046
surya-d-621 <suryadhanasekar621@gmail.com>
2025-02-19 23:02:50 +0530

    Count Goos Sub array Contribution Technique solved

--- DSA_Java/SlidingWindow.java
+++ DSA_Java/SlidingWindow.java
@@ -1,8 +1,8 @@
-package  DSA_Java;
+package DSA_Java;
 
 class SlidingWindow {
 
-    public static int findSubArrayWithLeastAverage(int[] A, int B){
+    public static int findSubArrayWithLeastAverage(int[] A, int B) {
 
         /*
 Problem Description: Given an array A of size N, find the subarray of size B with the least average.
@@ -45,7 +45,7 @@ Subarray between [4, 5] has minimum average
         int sum = 0;
         int arrLength = A.length;
 
-        for(int i=0; i< B; i++){
+        for (int i = 0; i < B; i++) {
             sum += A[i];
         }
 
@@ -53,11 +53,11 @@ Subarray between [4, 5] has minimum average
         int minIndex = 0;
 
 
-        for(int j = B; j < arrLength; j++){
+        for (int j = B; j < arrLength; j++) {
 
             //Here I didn't calculate average because if the sum is minimum there obviously the average will also be min.
             sum = sum + A[j] - A[j - B];
-            if(sum < minSum){
+            if (sum < minSum) {
                 minSum = sum;
                 minIndex = j - B + 1;
 
@@ -67,9 +67,84 @@ Subarray between [4, 5] has minimum average
         return minIndex;
     }
 
-    public static void main(String[] args){
+
+    public static int countGoodSubarrays(int[] A, int B) {
+ /*
+        Problem Description
+
+        Given an array of integers A, a subarray of an array is said to be good if it fulfills any one of the criteria:
+        1. Length of the subarray is be even, and the sum of all the elements of the subarray must be less than B.
+        2. Length of the subarray is be odd, and the sum of all the elements of the subarray must be greater than B.
+        Your task is to find the count of good subarrays in A.
+
+
+        Problem Constraints
+
+        1 <= len(A) <= 5 x 103
+        1 <= A[i] <= 103
+        1 <= B <= 107
+
+
+        Input Format
+
+        The first argument given is the integer array A.
+                The second argument given is an integer B.
+
+
+                Output Format
+
+        Return the count of good subarrays in A.
+
+
+        Example Input
+
+        Input 1:
+        A = [1, 2, 3, 4, 5]
+        B = 4
+        Input 2:
+
+        A = [13, 16, 16, 15, 9, 16, 2, 7, 6, 17, 3, 9]
+        B = 65
+
+
+        Example Output
+
+        Output 1:
+        6
+        Output 2:
+
+        36
+        */
+  
+        int n = A.length;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+
+            for (int j = i; j < n; j++) {
+                sum += A[j];
+                int length = j - i + 1;
+
+
+                if (length % 2 == 0 && sum < B) {
+                    count++;
+                } else if (length % 2 == 1 && sum > B) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
         int[] A1 = {3, 7, 90, 20, 10, 50, 40};
         int B1 = 3;
-        System.out.println("Output 1: " + findSubArrayWithLeastAverage(A1, B1));
+        System.out.println("findSubArrayWithLeastAverage: " + findSubArrayWithLeastAverage(A1, B1));
+
+
+        System.out.println("countGoodSubarrays: " + countGoodSubarrays(A1, B1));  // Expected: 6
     }
 }
